2d387f4708e87459d1976e6c3e13b2af6b15bd5f
manthanchauhan913 <manthanchauhan913@gmail.com>
2020-10-04 17:56:36 +0530

    broadcast server complete with respect to the website

--- server.py
+++ server.py
@@ -1,18 +1,34 @@
 import asyncio
 import websockets
 
+available_clients = set()
+
 
 # usual python function which waits until it receives
 # message from client, and then echos it.
 # it then sends a response
 async def response(websocket, path):
-    print(websocket)
-    await websocket.send("Hi client " + path)
-    await websocket.send("How are you?")
+    available_clients.add(websocket)
+    print("New client added:", path)
+
+    while True:
+        try:
+            msg = await websocket.recv()
+            await broadcast_to_clients(msg)
+        except websockets.ConnectionClosed:
+            available_clients.remove(websocket)
+            print("Terminated")
+            break
 
 
-start_server = websockets.serve(response, "localhost", 1234)
-asyncio.get_event_loop().run_until_complete(start_server)
-print("hi")
-asyncio.get_event_loop().run_forever()
-print(start_server)
+async def broadcast_to_clients(text):
+    for client_socket in available_clients:
+        await client_socket.send(text)
+
+
+start_server = websockets.serve(response, "192.168.0.13", 1234)
+
+loop = asyncio.get_event_loop()
+asyncio.ensure_future(start_server)
+
+loop.run_forever()
