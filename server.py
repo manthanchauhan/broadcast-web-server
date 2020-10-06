3dc5e96ef2a3c6ffe65d9dd159c902c5a0b2950d
manthanchauhan913 <manthanchauhan913@gmail.com>
2020-10-06 17:03:14 +0530

    added client authentication.

--- server.py
+++ server.py
@@ -1,22 +1,58 @@
 import asyncio
 import websockets
+import json
 
 available_clients = set()
+server_socket = None
+SERVER_KEY = "hy^3se%^GbuIoHEd"  # TODO move to env
 
 
 # usual python function which waits until it receives
 # message from client, and then echos it.
 # it then sends a response
 async def response(websocket, path):
-    available_clients.add(websocket)
-    print("New client added:", path)
+    global server_socket
+
+    auth_data = await websocket.recv()
+    auth_data = json.loads(auth_data)
+
+    auth_response = {
+        "success": False
+    }
+
+    if auth_data["type"] == "server":
+        if auth_data["password"] == SERVER_KEY:
+            auth_response["success"] = True
+
+    elif auth_data["type"] == "client":
+        auth_response["success"] = True
+
+    await websocket.send(json.dumps(auth_response))
+
+    if not auth_response["success"]:
+        return
+
+    if auth_data["type"] == "server":
+        server_socket = websocket
+        print("New Server added")
+    else:
+        available_clients.add(websocket)
+        print("New client added")
 
     while True:
         try:
             msg = await websocket.recv()
-            await broadcast_to_clients(msg)
+            print(msg)
+
+            if websocket is server_socket:
+                await broadcast_to_clients(msg)
+            else:
+                pass
         except websockets.ConnectionClosed:
-            available_clients.remove(websocket)
+            if websocket is server_socket:
+                server_socket = None
+            else:
+                available_clients.remove(websocket)
             print("Terminated")
             break
 
